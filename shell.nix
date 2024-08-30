fa2b424935ad2a8e29199b6064aff9e70575b18d
Christopher Johnstone <chrispjohnstone97@gmail.com>
2024-08-30 20:16:47 +0100

    Added shell.nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    packages = [
+      (
+        pkgs.python3.withPackages (
+          python-pkgs: [
+            python-pkgs.black
+          ]
+        )
+      )
+    ];
+  }
